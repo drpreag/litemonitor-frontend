31b472163cde32067923d6ba26459b4bfdbacce7
Predrag Vlajkovic <predrag.vlajkovic@gmail.com>
2018-05-23 14:01:39 +0200

    Fixed Line chart drawing

--- src/components/Charts/LineObservationsChart.vue
+++ src/components/Charts/LineObservationsChart.vue
@@ -10,15 +10,27 @@ export default {
       chartData: {
         labels: [],
         datasets: [
+          // {
+          //   label: ["Speed [ms]"],
+          //   data: [],            
+          //   backgroundColor: '#f87979',
+          //   backgroundColor: [
+          //       'rgba(255,99,132,1)',
+          //       'rgba(54, 162, 235, 1)'
+          //   ]
+          // }       
           {
-            label: ["Speed [ms]"],
-            data: [],            
-            backgroundColor: '#f87979',
-            backgroundColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)'
-            ]
-          }       
+            label: ['Down'],
+            borderColor: '#ff6384',
+            fill: false,
+            data: []
+          },
+          {
+            label: ['Up'],
+            borderColor: '#36a2eb',
+            fill: false,
+            data: []
+          }          
         ]    
       },    
       chartOptions: {
@@ -32,7 +44,12 @@ export default {
       var totalItems = this.observationData.length - 1;
       for (var i in this.observationData) {
         this.chartData.labels[totalItems-i] = this.observationData [i][0];
-        this.chartData.datasets[0].data[totalItems-i] = this.observationData [i][1];
+        if (this.observationData[i][1] == 0.0) {
+          this.chartData.datasets[0].data[totalItems-i] = this.observationData [i][1];
+        }
+        else {
+          this.chartData.datasets[1].data[totalItems-i] = this.observationData [i][1];
+        }
       }
       this.$data._chart.update();
     }
@@ -41,7 +58,12 @@ export default {
     var totalItems = this.observationData.length - 1;
     for (var i in this.observationData) {
       this.chartData.labels[totalItems-i] = this.observationData [i][0];
-      this.chartData.datasets[0].data[totalItems-i] = this.observationData [i][1];
+      if (this.observationData[i][1] == 0) {
+        this.chartData.datasets[0].data[totalItems-i] = this.observationData [i][1];
+      }
+      else {
+        this.chartData.datasets[1].data[totalItems-i] = this.observationData [i][1];
+      }      
     }
     this.renderChart(this.chartData, this.chartOptions);
   }
